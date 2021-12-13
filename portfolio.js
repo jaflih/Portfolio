76986d335a5f3b44c5bfef814ff55b9065946b0b
tobin <tobinchilongojr@gmail.com>
2021-12-13 15:22:11 +0200

    Add close menu when we click on the mobile menu

--- portfolio.js
+++ portfolio.js
@@ -5,3 +5,10 @@ function openMenu() {
   let mobile_navbar = document.querySelector(".navbar-mobile");
   mobile_navbar.classList.remove("navbar_hidden");
 }
+
+let navbar_mobile_close = document.querySelector(".navbar-mobile-close");
+navbar_mobile_close.addEventListener("click", closeMenu);
+function closeMenu() {
+  let mobile_navbar = document.querySelector(".navbar-mobile");
+  mobile_navbar.classList.add("navbar_hidden");
+}
