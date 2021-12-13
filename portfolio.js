c9999f3238a52332585b94e97ded373984e8e168
jihaneh <jijih@hotmail.fr>
2021-12-13 19:18:46 +0100

    Refactor js code

--- portfolio.js
+++ portfolio.js
@@ -1,18 +1,8 @@
 /* MOBILE MENU */
-function openMobileMenu() {
-  document.querySelector('.navbar_mobile').classList.remove('navbar_hidden');
-}
-
-function closeMobileMenu() {
-  document.querySelector('.navbar_mobile').classList.add('navbar_hidden');
+function toggleMobileMenu() {
+  document.querySelector(".navbar_mobile").classList.toggle("navbar_hidden");
 }
 
 document
-  .getElementById('js_open_menu')
-  .addEventListener('click', openMobileMenu);
-document
-  .querySelector('.js_close_menu')
-  .addEventListener('click', closeMobileMenu);
-document
-  .querySelectorAll('.nav_link_mobile')
-  .forEach((element) => element.addEventListener('click', closeMobileMenu));
+  .querySelectorAll("#js_open_menu, .js_close_menu, .nav_link_mobile")
+  .forEach((element) => element.addEventListener("click", toggleMobileMenu));
