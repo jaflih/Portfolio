fb1b8a42a7a64b193ece72517fabda7ee4d845ad
jihaneh <jijih@hotmail.fr>
2021-12-14 08:57:18 +0100

    Refactor js code and Update CSS and HTML homepage

--- portfolio.js
+++ portfolio.js
@@ -1,8 +1,4 @@
 /* MOBILE MENU */
-function toggleMobileMenu() {
-  document.querySelector(".navbar_mobile").classList.toggle("navbar_hidden");
-}
-
-document
-  .querySelectorAll("#js_open_menu, .js_close_menu, .nav_link_mobile")
-  .forEach((element) => element.addEventListener("click", toggleMobileMenu));
+document.querySelectorAll('.js_menu').forEach((element) => element.addEventListener('click', () => {
+  document.querySelector('.navbar_mobile').classList.toggle('navbar_hidden');
+}));
