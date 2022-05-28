a9bd2fc1812897af20a8ce371e9cffcfc83ceeed
jihaneh <jijih@hotmail.fr>
2022-05-28 19:29:29 +0200

    Add new project

--- projects.js
+++ projects.js
@@ -1,30 +1,29 @@
 const project1 = {
   index: 0,
+  name: 'Recipe App',
+  technologies: ['Ruby on Rails', 'CSS', 'Ruby', 'Rspec'],
+  image: 'recipe.png',
+  description: 'The Recipe app keeps track of all your recipes, ingredients, and inventory.',
+  version_link: 'https://secure-temple-71126.herokuapp.com/',
+  source_link: 'https://github.com/jaflih/recipe_app',
+};
+const project2 = {
+  index: 1,
   name: 'Capstone 1',
   technologies: ['HTML', 'CSS', 'Javascript'],
   image: 'capstone.png',
   description: 'Online website for a conference',
-  version_link: 'https://jihaneh.github.io/ScienceFestivalSite/',
-  source_link: 'https://github.com/jihaneH/ScienceFestivalSite',
+  version_link: 'https://jaflih.github.io/ScienceFestivalSite/',
+  source_link: 'https://github.com/jaflih/ScienceFestivalSite',
 };
-const project2 = {
-  index: 1,
+const project3 = {
+  index: 2,
   name: 'TodoList',
   technologies: ['HTML', 'CSS', 'Javascript'],
   image: 'todolist.png',
   description: "'To-do list' is a tool that helps to organize your day.",
-  version_link: 'https://jihaneh.github.io/TodoList/',
-  source_link: 'https://github.com/jihaneH/TodoList',
-};
-const project3 = {
-  index: 2,
-  name: 'Multi-Post Stories Gain+Glory',
-  technologies: ['Ruby on Rails', 'CSS', 'Javascript', 'Html'],
-  image: 'portfolio.png',
-  description:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-  version_link: 'https://github.com/',
-  source_link: 'https://github.com/',
+  version_link: 'https://jaflih.github.io/TodoList/',
+  source_link: 'https://github.com/jaflih/TodoList',
 };
 const project4 = {
   index: 3,
