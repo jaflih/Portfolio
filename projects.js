c9e9d483d59ec1ad21421353710f990ea171248c
jihaneh <jijih@hotmail.fr>
2022-07-19 09:32:56 +0200

    remove examples

--- projects.js
+++ projects.js
@@ -56,6 +56,6 @@ const project6 = {
   source_link: 'https://github.com/',
 };
 
-const projects = [project1, project2, project3, project4, project5, project6];
+const projects = [project1, project2, project3];
 
 export default projects;
